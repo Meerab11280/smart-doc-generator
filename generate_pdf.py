d9a99cd80dc704b37a94f67460f1b1c762129ee4
Meerab11280 <meerababid24@gmail.com>
2025-07-24 22:28:55 +0500

    Update generate_pdf.py

--- generate_pdf.py
+++ generate_pdf.py
@@ -4,6 +4,23 @@ from html import escape
 
 def create_pdf(text):
     result = BytesIO()
-    html = f"<pre>{escape(text)}</pre>"
+    html = f"""
+    <html>
+    <head>
+        <style>
+            pre {{
+                font-family: Arial, sans-serif;
+                font-size: 12pt;
+                line-height: 1.6;
+                margin: 30px;
+            }}
+        </style>
+    </head>
+    <body>
+        <pre>{escape(text)}</pre>
+    </body>
+    </html>
+    """
     pisa.CreatePDF(src=html, dest=result)
     return result.getvalue()
+
